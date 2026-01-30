ef0b79fdacf2c44bdb667c2da763048df0a1ea56
Alex hembrom <98451339+alex-hembrom@users.noreply.github.com>
2026-01-30 20:57:21 +0530

    changed brain.py

--- brain.py
+++ brain.py
@@ -9,7 +9,7 @@ GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # 2. Configure Gemini
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 # --- HELPER: FAKE DATA GENERATOR ---
 def generate_fake_data(data_type):
